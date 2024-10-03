d901cecd4d4e0498857369a0fa484e764a8bfb52
NickCulbertson <nickculbertson@gmail.com>
2024-10-03 19:36:56 -0400

    Reverted to original filter envelope

--- Sources/CDunneAudioKit/DunneCore/Sampler/SamplerVoice.cpp
+++ Sources/CDunneAudioKit/DunneCore/Sampler/SamplerVoice.cpp
@@ -185,164 +185,165 @@ bool SamplerVoice::getSamples(int sampleCount, float *leftOutput, float *rightOu
         pitchEnvelope.reset();
     }
 
-bool SamplerVoice::prepToGetSamples(int sampleCount, float masterVolume, float pitchOffset,
-                                    float cutoffMultiple, float keyTracking,
-                                    float cutoffEnvelopeStrength,
-                                    float cutoffEnvelopeVelocityScaling,
-                                    float resLinear, float pitchADSRSemitones,
-                                    float voiceLFODepthSemitones, float voiceLFOFrequencyHz)
-{
-    if (ampEnvelope.isIdle()) return true;
-
-    if (ampEnvelope.isPreStarting())
-    {
-        tempGain = masterVolume * tempNoteVolume;
-        volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
-        if (!ampEnvelope.isPreStarting())
-        {
-            tempGain = masterVolume * noteVolume;
-            volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
-            sampleBuffer = newSampleBuffer;
-            oscillator.increment = (sampleBuffer->sampleRate / samplingRate) * (noteFrequency / sampleBuffer->noteFrequency);
-            oscillator.indexPoint = sampleBuffer->startPoint;
-            oscillator.isLooping = sampleBuffer->isLooping;
-        }
-    }
-    else
-    {
-        tempGain = masterVolume * noteVolume;
-        volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
-    }
-
-    if (*glideSecPerOctave != 0.0f && glideSemitones != 0.0f)
-    {
-        float seconds = sampleCount / samplingRate;
-        float semitones = 12.0f * seconds / *glideSecPerOctave;
-        if (glideSemitones < 0.0f)
-        {
-            glideSemitones += semitones;
-            if (glideSemitones > 0.0f) glideSemitones = 0.0f;
-        }
-        else
-        {
-            glideSemitones -= semitones;
-            if (glideSemitones < 0.0f) glideSemitones = 0.0f;
-        }
-    }
-
-    float pitchCurveAmount = 1.0f; // >1 = faster curve, 0 < curve < 1 = slower curve - make this a parameter
-    if (pitchCurveAmount < 0) { pitchCurveAmount = 0; }
-    pitchEnvelopeSemitones = pow(pitchEnvelope.getSample(), pitchCurveAmount) * pitchADSRSemitones;
-
-    vibratoLFO.setFrequency(voiceLFOFrequencyHz);
-    voiceLFOSemitones = vibratoLFO.getSample() * voiceLFODepthSemitones;
-
-    float pitchOffsetModified = pitchOffset + glideSemitones + pitchEnvelopeSemitones + voiceLFOSemitones;
-    oscillator.setPitchOffsetSemitones(pitchOffsetModified);
-
-    // Negative value of cutoffMultiple means filters are disabled
-    if (cutoffMultiple < 0.0f)
-    {
-        isFilterEnabled = false;
-    }
-    else
-    {
-        isFilterEnabled = true;
-
-        // Calculate the base cutoff frequency based on the note frequency and key tracking
-        float noteHz = noteFrequency * powf(2.0f, (pitchOffsetModified) / 12.0f);
-        float baseFrequency = MIDDLE_C_HZ + keyTracking * (noteHz - MIDDLE_C_HZ);
-
-        // Modify the cutoff frequency calculation to take the envelope into account more directly
-        float envValue = filterEnvelope.getSample(); // Typically ranges from 0.0 to 1.0
-        double cutoffFrequency = baseFrequency * cutoffMultiple * envValue;
-
-        // Ensure cutoff frequency stays within a reasonable range
-        if (cutoffFrequency > samplingRate / 2.0f) // Nyquist frequency limit
-            cutoffFrequency = samplingRate / 2.0f;
-
-        // Set the filter parameters for both left and right channels
-        leftFilter.setParameters(cutoffFrequency, resLinear);
-        rightFilter.setParameters(cutoffFrequency, resLinear);
-    }
-
-    return false;
-}
-// Previous envelope
-//    bool SamplerVoice::prepToGetSamples(int sampleCount, float masterVolume, float pitchOffset,
-//                                        float cutoffMultiple, float keyTracking,
-//                                        float cutoffEnvelopeStrength, float cutoffEnvelopeVelocityScaling,
-//                                        float resLinear, float pitchADSRSemitones,
-//                                        float voiceLFODepthSemitones, float voiceLFOFrequencyHz)
-//    {
-//        if (ampEnvelope.isIdle()) return true;
+//bool SamplerVoice::prepToGetSamples(int sampleCount, float masterVolume, float pitchOffset,
+//                                    float cutoffMultiple, float keyTracking,
+//                                    float cutoffEnvelopeStrength,
+//                                    float cutoffEnvelopeVelocityScaling,
+//                                    float resLinear, float pitchADSRSemitones,
+//                                    float voiceLFODepthSemitones, float voiceLFOFrequencyHz)
+//{
+//    if (ampEnvelope.isIdle()) return true;
 //
-//        if (ampEnvelope.isPreStarting())
-//        {
-//            tempGain = masterVolume * tempNoteVolume;
-//            volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
-//            // This can execute as part of the voice-stealing mechanism, and will be executed rarely.
-//            // To test, set MAX_POLYPHONY in CoreSampler.cpp to something small like 2 or 3.
-//            if (!ampEnvelope.isPreStarting())
-//            {
-//                tempGain = masterVolume * noteVolume;
-//                volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
-//                sampleBuffer = newSampleBuffer;
-//                oscillator.increment = (sampleBuffer->sampleRate / samplingRate) * (noteFrequency / sampleBuffer->noteFrequency);
-//                oscillator.indexPoint = sampleBuffer->startPoint;
-//                oscillator.isLooping = sampleBuffer->isLooping;
-//            }
-//        }
-//        else
+//    if (ampEnvelope.isPreStarting())
+//    {
+//        tempGain = masterVolume * tempNoteVolume;
+//        volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
+//        if (!ampEnvelope.isPreStarting())
 //        {
 //            tempGain = masterVolume * noteVolume;
 //            volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
+//            sampleBuffer = newSampleBuffer;
+//            oscillator.increment = (sampleBuffer->sampleRate / samplingRate) * (noteFrequency / sampleBuffer->noteFrequency);
+//            oscillator.indexPoint = sampleBuffer->startPoint;
+//            oscillator.isLooping = sampleBuffer->isLooping;
 //        }
+//    }
+//    else
+//    {
+//        tempGain = masterVolume * noteVolume;
+//        volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
+//    }
 //
-//        if (*glideSecPerOctave != 0.0f && glideSemitones != 0.0f)
+//    if (*glideSecPerOctave != 0.0f && glideSemitones != 0.0f)
+//    {
+//        float seconds = sampleCount / samplingRate;
+//        float semitones = 12.0f * seconds / *glideSecPerOctave;
+//        if (glideSemitones < 0.0f)
 //        {
-//            float seconds = sampleCount / samplingRate;
-//            float semitones = 12.0f * seconds / *glideSecPerOctave;
-//            if (glideSemitones < 0.0f)
-//            {
-//                glideSemitones += semitones;
-//                if (glideSemitones > 0.0f) glideSemitones = 0.0f;
-//            }
-//            else
-//            {
-//                glideSemitones -= semitones;
-//                if (glideSemitones < 0.0f) glideSemitones = 0.0f;
-//            }
-//        }
-//
-//        float pitchCurveAmount = 1.0f; // >1 = faster curve, 0 < curve < 1 = slower curve - make this a parameter
-//        if (pitchCurveAmount < 0) { pitchCurveAmount = 0; }
-//        pitchEnvelopeSemitones = pow(pitchEnvelope.getSample(), pitchCurveAmount) * pitchADSRSemitones;
-//
-//        vibratoLFO.setFrequency(voiceLFOFrequencyHz);
-//        voiceLFOSemitones = vibratoLFO.getSample() * voiceLFODepthSemitones;
-//
-//        float pitchOffsetModified = pitchOffset + glideSemitones + pitchEnvelopeSemitones + voiceLFOSemitones;
-//        oscillator.setPitchOffsetSemitones(pitchOffsetModified);
-//
-//        // negative value of cutoffMultiple means filters are disabled
-//        if (cutoffMultiple < 0.0f)
-//        {
-//            isFilterEnabled = false;
+//            glideSemitones += semitones;
+//            if (glideSemitones > 0.0f) glideSemitones = 0.0f;
 //        }
 //        else
 //        {
-//            isFilterEnabled = true;
-//            float noteHz = noteFrequency * powf(2.0f, (pitchOffsetModified) / 12.0f);
-//            float baseFrequency = MIDDLE_C_HZ + keyTracking * (noteHz - MIDDLE_C_HZ);
-//            float envStrength = ((1.0f - cutoffEnvelopeVelocityScaling) + cutoffEnvelopeVelocityScaling * noteVolume);
-//            double cutoffFrequency = baseFrequency * (1.0f + cutoffMultiple + cutoffEnvelopeStrength * envStrength * filterEnvelope.getSample());
-//            leftFilter.setParameters(cutoffFrequency, resLinear);
-//            rightFilter.setParameters(cutoffFrequency, resLinear);
+//            glideSemitones -= semitones;
+//            if (glideSemitones < 0.0f) glideSemitones = 0.0f;
 //        }
-//
-//        return false;
 //    }
+//
+//    float pitchCurveAmount = 1.0f; // >1 = faster curve, 0 < curve < 1 = slower curve - make this a parameter
+//    if (pitchCurveAmount < 0) { pitchCurveAmount = 0; }
+//    pitchEnvelopeSemitones = pow(pitchEnvelope.getSample(), pitchCurveAmount) * pitchADSRSemitones;
+//
+//    vibratoLFO.setFrequency(voiceLFOFrequencyHz);
+//    voiceLFOSemitones = vibratoLFO.getSample() * voiceLFODepthSemitones;
+//
+//    float pitchOffsetModified = pitchOffset + glideSemitones + pitchEnvelopeSemitones + voiceLFOSemitones;
+//    oscillator.setPitchOffsetSemitones(pitchOffsetModified);
+//
+//    // Negative value of cutoffMultiple means filters are disabled
+//    if (cutoffMultiple < 0.0f)
+//    {
+//        isFilterEnabled = false;
+//    }
+//    else
+//    {
+//        isFilterEnabled = true;
+//
+//        // Calculate the base cutoff frequency based on the note frequency and key tracking
+//        float noteHz = noteFrequency * powf(2.0f, (pitchOffsetModified) / 12.0f);
+//        float baseFrequency = MIDDLE_C_HZ + keyTracking * (noteHz - MIDDLE_C_HZ);
+//
+//        // Modify the cutoff frequency calculation to take the envelope into account more directly
+//        float envValue = filterEnvelope.getSample(); // Typically ranges from 0.0 to 1.0
+//        double cutoffFrequency = baseFrequency * cutoffMultiple * envValue;
+//
+//        // Ensure cutoff frequency stays within a reasonable range
+//        if (cutoffFrequency > samplingRate / 2.0f) // Nyquist frequency limit
+//            cutoffFrequency = samplingRate / 2.0f;
+//
+//        // Set the filter parameters for both left and right channels
+//        leftFilter.setParameters(cutoffFrequency, resLinear);
+//        rightFilter.setParameters(cutoffFrequency, resLinear);
+//    }
+//
+//    return false;
+//}
+
+// Envelope with cutoffEnvelopeStrength
+    bool SamplerVoice::prepToGetSamples(int sampleCount, float masterVolume, float pitchOffset,
+                                        float cutoffMultiple, float keyTracking,
+                                        float cutoffEnvelopeStrength, float cutoffEnvelopeVelocityScaling,
+                                        float resLinear, float pitchADSRSemitones,
+                                        float voiceLFODepthSemitones, float voiceLFOFrequencyHz)
+    {
+        if (ampEnvelope.isIdle()) return true;
+
+        if (ampEnvelope.isPreStarting())
+        {
+            tempGain = masterVolume * tempNoteVolume;
+            volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
+            // This can execute as part of the voice-stealing mechanism, and will be executed rarely.
+            // To test, set MAX_POLYPHONY in CoreSampler.cpp to something small like 2 or 3.
+            if (!ampEnvelope.isPreStarting())
+            {
+                tempGain = masterVolume * noteVolume;
+                volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
+                sampleBuffer = newSampleBuffer;
+                oscillator.increment = (sampleBuffer->sampleRate / samplingRate) * (noteFrequency / sampleBuffer->noteFrequency);
+                oscillator.indexPoint = sampleBuffer->startPoint;
+                oscillator.isLooping = sampleBuffer->isLooping;
+            }
+        }
+        else
+        {
+            tempGain = masterVolume * noteVolume;
+            volumeRamper.reinit(ampEnvelope.getSample(), sampleCount);
+        }
+
+        if (*glideSecPerOctave != 0.0f && glideSemitones != 0.0f)
+        {
+            float seconds = sampleCount / samplingRate;
+            float semitones = 12.0f * seconds / *glideSecPerOctave;
+            if (glideSemitones < 0.0f)
+            {
+                glideSemitones += semitones;
+                if (glideSemitones > 0.0f) glideSemitones = 0.0f;
+            }
+            else
+            {
+                glideSemitones -= semitones;
+                if (glideSemitones < 0.0f) glideSemitones = 0.0f;
+            }
+        }
+
+        float pitchCurveAmount = 1.0f; // >1 = faster curve, 0 < curve < 1 = slower curve - make this a parameter
+        if (pitchCurveAmount < 0) { pitchCurveAmount = 0; }
+        pitchEnvelopeSemitones = pow(pitchEnvelope.getSample(), pitchCurveAmount) * pitchADSRSemitones;
+
+        vibratoLFO.setFrequency(voiceLFOFrequencyHz);
+        voiceLFOSemitones = vibratoLFO.getSample() * voiceLFODepthSemitones;
+
+        float pitchOffsetModified = pitchOffset + glideSemitones + pitchEnvelopeSemitones + voiceLFOSemitones;
+        oscillator.setPitchOffsetSemitones(pitchOffsetModified);
+
+        // negative value of cutoffMultiple means filters are disabled
+        if (cutoffMultiple < 0.0f)
+        {
+            isFilterEnabled = false;
+        }
+        else
+        {
+            isFilterEnabled = true;
+            float noteHz = noteFrequency * powf(2.0f, (pitchOffsetModified) / 12.0f);
+            float baseFrequency = MIDDLE_C_HZ + keyTracking * (noteHz - MIDDLE_C_HZ);
+            float envStrength = ((1.0f - cutoffEnvelopeVelocityScaling) + cutoffEnvelopeVelocityScaling * noteVolume);
+            double cutoffFrequency = baseFrequency * (1.0f + cutoffMultiple + cutoffEnvelopeStrength * envStrength * filterEnvelope.getSample());
+            leftFilter.setParameters(cutoffFrequency, resLinear);
+            rightFilter.setParameters(cutoffFrequency, resLinear);
+        }
+
+        return false;
+    }
 
     void SamplerVoice::restartVoiceLFOIfNeeded() {
         if (restartVoiceLFO || !hasStartedVoiceLFO) {
